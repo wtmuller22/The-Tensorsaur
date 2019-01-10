22e03d853b60aa7bc1d5d6c31e2bde827f8e0afc
Wyatt Muller <wtmuller22@vt.edu>
2019-01-10 00:34:14 -0500

    Changed python interpreter version to 3.6.8 in order to install tensorflow and other packages. Adds working dataset.

--- game/tensor.py
+++ game/tensor.py
@@ -7,12 +7,23 @@ Created on Dec 31, 2018
 
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 
 #7 params
 #{distance to ob, height of ob, width of ob, bird height, speed, player y pos, gap between obs}
 #3 lables
 #0 = stay, 1 = jump, 2 = duck
-input = np.array([[1,1,1,1,1,1,1],[2,2,2,2,2,2,2]], np.int32)
-labels = np.array([0,2])
+distance_to = pd.Series([])
+height_of = pd.Series([])
+width_of = pd.Series([])
+obstacle_y = pd.Series([])
+speed = pd.Series([])
+player_y = pd.Series([])
+obstacle_gap = pd.Series([])
+labels = pd.Series([])
 
-dataset = tf.data.Dataset.from_tensor_slices((input,labels))
+playing_data = pd.DataFrame({ 'Distance to Obstacle': distance_to, 'Height of Obstacle': height_of,
+                             'Width of Obstacle': width_of, 'Obstacle y Position': obstacle_y, 
+                             'Speed': speed, 'Player y Position': player_y, 
+                             'Gap Between Obstacles': obstacle_gap, 'Player State': labels})
+playing_data
