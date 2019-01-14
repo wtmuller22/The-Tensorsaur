ff2203fe997904f0db537660f1d5c0b67e643f69
Wyatt Muller <wtmuller22@vt.edu>
2019-01-14 15:08:38 -0500

    Changes model to a multi-class linear classifier using logistic regression to produce discrete predictions. Current hyperparameters should be about 83% accurate.

--- game/tensor.py
+++ game/tensor.py
@@ -75,10 +75,10 @@ def preprocess_targets(playing_data):
     output_targets["player_state"] = playing_data["player_state"]
     return output_targets
 
-training_examples = preprocess_features(playing_data.head(60000))
-training_targets = preprocess_targets(playing_data.head(60000))
-validation_examples = preprocess_features(playing_data.tail(20000))
-validation_targets = preprocess_targets(playing_data.tail(20000))
+training_examples = preprocess_features(playing_data.head(5009))
+training_targets = preprocess_targets(playing_data.head(5009))
+validation_examples = preprocess_features(playing_data.tail(1000))
+validation_targets = preprocess_targets(playing_data.tail(1000))
 
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     features = {key:np.array(value) for key,value in dict(features).items()}
@@ -96,43 +96,54 @@ def train_model(learning_rate, steps, batch_size, training_examples, training_ta
     periods = 10
     steps_per_period = steps / periods
     
-    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
     my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
-    linear_regressor = tf.estimator.LinearRegressor(feature_columns=construct_feature_columns(training_examples),
-                                                    optimizer=my_optimizer)
+    linear_classifier = tf.estimator.LinearClassifier(feature_columns=construct_feature_columns(training_examples),
+                                                    optimizer=my_optimizer, n_classes=3, 
+                                                    config=tf.estimator.RunConfig(keep_checkpoint_max=1))
     
     training_input_fn = lambda: my_input_fn(training_examples, training_targets["player_state"], batch_size=batch_size)
     predict_training_input_fn = lambda: my_input_fn(training_examples, training_targets["player_state"], shuffle=False, num_epochs=1)
     predict_validation_input_fn = lambda: my_input_fn(validation_examples, validation_targets["player_state"], shuffle=False, num_epochs=1)
     
     print("Training in Progress...")
-    print("RMSE (training data):")
-    training_rmse = []
-    validation_rmse = []
+    print("LogLoss (training data):")
+    training_log_losses = []
+    validation_log_losses = []
     for period in range(0, periods):
-        linear_regressor.train(input_fn=training_input_fn, steps=steps_per_period)
+        linear_classifier.train(input_fn=training_input_fn, steps=steps_per_period)
         
-        training_predictions = linear_regressor.predict(input_fn=predict_training_input_fn)
-        training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+        training_predictions = list(linear_classifier.predict(input_fn=predict_training_input_fn))
+        training_probabilities = np.array([item['probabilities'] for item in training_predictions])
+        training_pred_class_id = np.array([item['class_ids'][0] for item in training_predictions])
+        training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id,3)
+            
+        validation_predictions = list(linear_classifier.predict(input_fn=predict_validation_input_fn))
+        validation_probabilities = np.array([item['probabilities'] for item in validation_predictions])    
+        validation_pred_class_id = np.array([item['class_ids'][0] for item in validation_predictions])
+        validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id,3)
         
-        validation_predictions = linear_regressor.predict(input_fn=predict_validation_input_fn)
-        validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
-        
-        training_root_mean_squared_error = math.sqrt(metrics.mean_squared_error(training_predictions, training_targets))
-        validation_root_mean_squared_error = math.sqrt(metrics.mean_squared_error(validation_predictions, validation_targets))
-        print("Period %02d : %0.2f" % (period, training_root_mean_squared_error))
-        print("Validation: %0.2f" % (validation_root_mean_squared_error))
-        training_rmse.append(training_root_mean_squared_error)
-        validation_rmse.append(validation_root_mean_squared_error)
+        training_log_loss = metrics.log_loss(training_targets, training_pred_one_hot)
+        validation_log_loss = metrics.log_loss(validation_targets, validation_pred_one_hot)
+        print("Period %02d : %0.2f" % (period, training_log_loss))
+        print("Validation: %0.2f" % (validation_log_loss))
+        training_log_losses.append(training_log_loss)
+        validation_log_losses.append(validation_log_loss)
     print("Model Training Complete.")
     
-    return linear_regressor
+    final_predictions = linear_classifier.predict(input_fn=predict_validation_input_fn)
+    final_predictions = np.array([item['class_ids'][0] for item in final_predictions])
+    
+    accuracy = metrics.accuracy_score(validation_targets, final_predictions)
+    print("Final accuracy (validation data): %0.2f" % accuracy)
+    
+    return linear_classifier
 
 #train model
 
-linear_regressor = train_model(learning_rate=0.00002,
-                                steps=20, 
-                                batch_size=10,
+linear_classifier = train_model(learning_rate=0.0001,
+                                steps=2000, 
+                                batch_size=100,
                                 training_examples=training_examples, 
                                 training_targets=training_targets, 
                                 validation_examples=validation_examples, 
