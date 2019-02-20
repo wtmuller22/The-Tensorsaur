85c28e35a9eb40bb773616532e53e706f20d629d
Chapman <cpendery@vt.edu>
2019-02-20 17:15:35 -0500

    Should be set to make a rectangle

--- QLearning/dino_game/envs/dino_env.py
+++ QLearning/dino_game/envs/dino_env.py
@@ -3,16 +3,39 @@ import gym
 from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
+import pdb
 #from dino_dynam import Dinosaur
 
-class DinoEnv(gym.Env, utils.EzPickle):
-    metadata = {'render.modes': ['human']}
+class DinoEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second' : 50
+    }
 
     def __init__(self):
         self.gravity = 9.8
-        self.dinomass = 1.0
+        self.masscart = 1.0
+        #self.masspole = 0.1
+        #self.total_mass = (self.masspole + self.masscart)
+        #self.length = 0.5 # actually half the pole's length
+        #self.polemass_length = (self.masspole * self.length)
+        #self.force_mag = 10.0
+        self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = 'euler'
-        self.tau = 0.02
+
+        # # Angle at which to fail the episode
+        # self.theta_threshold_radians = 12 * 2 * math.pi / 360
+        # self.x_threshold = 2.4
+
+        # # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
+        # high = np.array([
+        #     self.x_threshold * 2,
+        #     np.finfo(np.float32).max,
+        #     self.theta_threshold_radians * 2,
+        #     np.finfo(np.float32).max])
+
+        self.action_space = spaces.Discrete(2)
+        # self.observation_space = spaces.Box(-high, high, dtype=np.float32)
 
         self.seed()
         self.viewer = None
@@ -25,24 +48,7 @@ class DinoEnv(gym.Env, utils.EzPickle):
         return [seed]
 
     def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
-        state = self.state
-        
-        #minus for every frame, minus bit for hit, plus big for jump in range, minus for jump out of ran
-
-        if not done:
-            reward = 1.0
-        elif self.steps_beyond_done is None:
-            # Pole just fell!
-            self.steps_beyond_done = 0
-            reward = 1.0
-        else:
-            if self.steps_beyond_done == 0:
-                logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
-            self.steps_beyond_done += 1
-            reward = 0.0
-
-        return np.array(self.state), reward, done, {}
+        print('hi')
 
     def reset(self):
         self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
@@ -53,20 +59,54 @@ class DinoEnv(gym.Env, utils.EzPickle):
         screen_width = 600
         screen_height = 400
 
-        dinowidth = 20.0
-        dinoheight = 100.0
-
-        world_width = self.x_threshold*2
+        world_width = 60#self.x_threshold*2
         scale = screen_width/world_width
+        carty = 100 # TOP OF CART
+        #polewidth = 10.0
+        #polelen = scale * (2 * self.length)
+        cartwidth = 30.0
+        cartheight = 60.0
 
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            l,r,t,b = -dinowidth/2, dinowidth/2, dinoheight/2, -dinoheight/2
-            dino = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            self.viewer.add_geom(dino)
+            l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
+            #axleoffset =cartheight/4.0
+            cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+            self.carttrans = rendering.Transform()
+            cart.add_attr(self.carttrans)
+            self.viewer.add_geom(cart)
+            # l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
+            # pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+            # pole.set_color(.8,.6,.4)
+            # self.poletrans = rendering.Transform(translation=(0, axleoffset))
+            # pole.add_attr(self.poletrans)
+            # pole.add_attr(self.carttrans)
+            # self.viewer.add_geom(pole)
+            # self.axle = rendering.make_circle(polewidth/2)
+            # self.axle.add_attr(self.poletrans)
+            # self.axle.add_attr(self.carttrans)
+            # self.axle.set_color(.5,.5,.8)
+            # self.viewer.add_geom(self.axle)
+            self.track = rendering.Line((0,carty-60), (screen_width,carty-60))
+            self.track.set_color(0,0,0)
+            self.viewer.add_geom(self.track)
+
+            # self._pole_geom = pole
 
         if self.state is None: return None
+
+        # Edit the pole polygon vertex
+        #pole = self._pole_geom
+        #l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
+        #pole.v = [(l,b), (l,t), (r,t), (r,b)]
+
+        x = self.state
+        cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
+        self.carttrans.set_translation(cartx, carty-30)
+        print(carty)
+        print(cartx)
+        #self.poletrans.set_rotation(-x[2])
 
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
